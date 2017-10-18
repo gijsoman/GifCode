67cfc59320f645e454dcf538fcc36e0b83493473
gijsoman <gijsbakker1@hotmail.com>
2017-10-18 13:59:56 +0200

    Added some error handling

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -47,21 +47,52 @@ public class DialogueManager : MonoBehaviour {
     public void OnClick(Button button)
     {
         //currentQuestion = questions[int.Parse(button.name.Substring(button.name.Length - 2))];
-        if (button.name == "Option1")
+        //added some errorhandling  if there is no text on the button there is no answer so i dont want to set my currentquestion
+        if (button.name == "Option1" && questions.Count > 1 && userOptionsText[0].text != "")
         {
-            currentQuestion = questions.First(question => question.id == currentQuestion.answers[0].answerId);
+            //if there are more questions and the id of my answer isnt bigger than the total amount of questions we want to set currentQuestion.
+            //Otherwise we would be leading to questions that don't exist.
+            if (currentQuestion.answers[0].answerId <= questions.Count - 1)
+            {
+                currentQuestion = questions.First(question => question.id == currentQuestion.answers[0].answerId);
+            }
+            else
+            {
+                Debug.Log("Dit antwoord heeft geen volgende vraag");
+            }
         }
-        if (button.name == "Option2")
+        if (button.name == "Option2" && questions.Count > 1 && userOptionsText[1].text != "")
         {
-            currentQuestion = questions.First(question => question.id == currentQuestion.answers[1].answerId);
+            if (currentQuestion.answers[1].answerId <= questions.Count - 1)
+            {
+                currentQuestion = questions.First(question => question.id == currentQuestion.answers[1].answerId);
+            }
+            else
+            {
+                Debug.Log("Dit antwoord heeft geen volgende vraag");
+            }
         }
-        if (button.name == "Option3")
+        if (button.name == "Option3" && questions.Count > 1 && userOptionsText[2].text != "")
         {
-            currentQuestion = questions.First(question => question.id == currentQuestion.answers[2].answerId);
+            if (currentQuestion.answers[2].answerId <= questions.Count - 1)
+            {
+                currentQuestion = questions.First(question => question.id == currentQuestion.answers[2].answerId);
+            }
+            else
+            {
+                Debug.Log("Dit antwoord heeft geen volgende vraag");
+            }
         }
-        if (button.name == "Option4")
+        if (button.name == "Option4" && questions.Count > 1 && userOptionsText[3].text != "")
         {
-            currentQuestion = questions.First(question => question.id == currentQuestion.answers[3].answerId);
+            if (currentQuestion.answers[3].answerId <= questions.Count - 1)
+            {
+                currentQuestion = questions.First(question => question.id == currentQuestion.answers[3].answerId);
+            }
+            else
+            {
+                Debug.Log("Dit antwoord heeft geen volgende vraag");
+            }
         }
 
         SetText();
