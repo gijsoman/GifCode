ba5e56168c4cd41fe72c81e1e3f9321db75615f4
gijsoman <gijsbakker1@hotmail.com>
2017-10-19 11:11:49 +0200

    better file reading.

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Answer
+{
+    public string answerText;
+    public int answerId;
+
+    public Answer(int answerId, string answerText)
+    {
+        this.answerText = answerText;
+        this.answerId = answerId;
+    }
+}
---
+++ Assets/Scripts/QuestionInitializer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public static class QuestionInitializer
+{
+    public static List<Question> InitializeQuestions()
+    {
+        //Hier mag je questions initializen en vanuit JSon halen, voorbeeld van hoe het misschien kan.
+        string[] linar = new string[10];
+        List<Question> questions = new List<Question>();
+
+        using (StreamReader reader = new StreamReader("Assets/Scripts/flitsScenarioAgressief.txt", Encoding.Default))
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                linar = line.Split('|');
+                List<Answer> answers = new List<Answer>();
+                for (int i = 2; i < linar.Length - 1; i += 2)
+                {
+                    answers.Add(new Answer(int.Parse(linar[i]), linar[i + 1]));
+                }
+                questions.Add(new Question(int.Parse(linar[0]), linar[1], answers));
+            }
+        }
+
+        return questions;
+    }
+}
