8e00357111833f1d331e397870adbf4867049eb6
gijsoman <gijsbakker1@hotmail.com>
2017-10-23 15:23:34 +0200

    UI menu slidein. Boos character. text placement

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject menu;
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        menu = GameObject.Find("Menu");
+        animator = menu.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void OnClick(Button button)
+    {
+        if (button.name == "MenuBurger")
+        {
+            animator.SetBool("SlideOut", false);
+            animator.SetBool("SlideIn", true);
+        }
+        if (button.name == "MenuCross")
+        {
+            animator.SetBool("SlideIn", false);
+            animator.SetBool("SlideOut", true);
+        }
+    }
+}
