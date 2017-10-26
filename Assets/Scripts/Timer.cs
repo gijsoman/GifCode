bad1b5a1587a0aa9d1d76c584c754c2b01709e41
gijsoman <gijsbakker1@hotmail.com>
2017-10-26 11:57:00 +0200

    Renewed timer.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,46 +6,28 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    //We can give in a time in the editor
     public float timePerQuestion;
     public int amountOfDots;
 
-    //We want a local variable to keep track of the time and not the in editor variable when we set the timer again.
     float currentTime;
     Text timerText;
 
-    //We need a dot prefab and a list where we put instantiated dots in.
     public GameObject dotPrefab;
-    List<GameObject> dots = new List<GameObject>();
 
-    //some variables for dot placement.
     RectTransform timerRect;
     RectTransform dotRect;
-
-    //we need to check if the timer is set before we start removing dots.
-    // bool timerSet;
-
-    //to keep track of the last time a dot was removed
-    float lastTime;
-
-    //to figure out how much time a dot has for lifetime
-    float timePerDot;
-
-    //to keep track of the amount of disabled dots
-    int amountOfEnabledDots;
-
-    //We need to edit parameters of the animator
-    List<Animator> animators = new List<Animator>();
 
     void Awake()
     {
         timerRect = this.gameObject.GetComponent<RectTransform>();
+        UITimer.amountOfDots = amountOfDots;
         CreateDots(dotPrefab);
     }
+
     void Start()
     {
         timerText = this.gameObject.GetComponentInChildren<Text>();
-        timePerDot = timePerQuestion / amountOfDots;
+        UITimer.timePerDot = timePerQuestion / amountOfDots;
     }
 
     void Update()
@@ -54,7 +36,7 @@ public class Timer : MonoBehaviour
         {
             currentTime -= Time.deltaTime;
             timerText.text = currentTime.ToString("f0");
-            DisableDotsOnTime();
+            UITimer.DisableDotsOnTime(currentTime);
         }
         else
         {
@@ -66,19 +48,18 @@ public class Timer : MonoBehaviour
 
     public void SetTimer()
     {
-        ResetAnimatorBools();
-        EnableAllDots();
-        amountOfEnabledDots = amountOfDots;
-        currentTime = timePerQuestion;
-        lastTime = currentTime;
+        UITimer.ResetAnimatorBools();
+        UITimer.EnableAllDots();
+        UITimer.amountOfEnabledDots = amountOfDots;
+        UITimer.lastTime = currentTime = timePerQuestion;
     }
 
     bool TimeUp()
     { 
         if (currentTime <= 0)
         {
-            DisableAllDots();
-            ResetAnimatorBools();
+            UITimer.DisableAllDots();
+            UITimer.ResetAnimatorBools();
             return true;
         }
         else{ return false; }
@@ -86,56 +67,16 @@ public class Timer : MonoBehaviour
 
     void CreateDots(GameObject dotPrefab)
     {
+        UITimer.dots.Clear();
+        UITimer.animators.Clear();
         for (int i = 0; i < amountOfDots; i++)
         {
             GameObject dot = Instantiate(dotPrefab) as GameObject;
-            dots.Add(dot);
-            animators.Add(dot.GetComponent<Animator>());
+            UITimer.dots.Add(dot);
+            UITimer.animators.Add(dot.GetComponent<Animator>());
             dotRect = dot.GetComponent<RectTransform>();
             dot.transform.parent = this.transform;
             dot.transform.localPosition = new Vector2(timerRect.rect.x + dotRect.rect.size.x / 2 + (dotRect.rect.size.x * i), timerRect.rect.y + dotRect.rect.size.y / 2);
-        }
-    }
-
-    void DisableAllDots()
-    {
-        for (int i = 0; i < amountOfDots; i++)
-        {
-            dots[i].gameObject.SetActive(false);
-        }
-    }
-
-    void EnableAllDots()
-    {
-        for (int i = 0; i < amountOfDots; i++)
-        {
-            dots[i].gameObject.SetActive(true);
-        }
-    }
-
-    void DisableDotsOnTime()
-    {
-        if (currentTime <= lastTime - timePerDot)
-        {
-            dots[amountOfEnabledDots - 1].gameObject.SetActive(false);
-            lastTime = currentTime;
-            amountOfEnabledDots--;
-        }
-        if (currentTime <= lastTime - timePerDot + 0.9)
-        {
-            if (animators[amountOfEnabledDots -1].GetBool("MayBreak") != true)
-            {
-                animators[amountOfEnabledDots - 1].SetBool("MayBreak", true);
-            }
-        }
-    }
-
-    void ResetAnimatorBools()
-    {
-        for (int i = 0; i < amountOfDots; i++)
-        {
-            animators[i].SetBool("MayBreak", false);
-            animators[i].Rebind();
         }
     }
 }
---
+++ Assets/Scripts/UITimer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class UITimer
+{
+    public static float timePerDot;
+    public static List<GameObject> dots = new List<GameObject>();
+
+    public static int amountOfEnabledDots;
+
+    public static List<Animator> animators = new List<Animator>();
+
+    public static int amountOfDots;
+
+    public static float lastTime;
+
+    public static void DisableAllDots()
+    {
+        for (int i = 0; i < dots.Count; i++)
+        {
+            dots[i].gameObject.SetActive(false);
+        }
+    }
+
+    public static void EnableAllDots()
+    {
+        for (int i = 0; i < dots.Count; i++)
+        {
+            dots[i].gameObject.SetActive(true);
+        }
+    }
+
+    public static void DisableDotsOnTime(float currentTime)
+    {
+        if (currentTime <= lastTime - timePerDot)
+        {
+            dots[amountOfEnabledDots - 1].gameObject.SetActive(false);
+            lastTime = currentTime;
+            amountOfEnabledDots--;
+        }
+        if (currentTime <= lastTime - timePerDot + 0.9)
+        {
+            if (animators[amountOfEnabledDots - 1].GetBool("MayBreak") != true)
+            {
+                animators[amountOfEnabledDots - 1].SetBool("MayBreak", true);
+            }
+        }
+    }
+
+    public static void ResetAnimatorBools()
+    {
+        for (int i = 0; i < amountOfDots; i++)
+        {
+            animators[i].SetBool("MayBreak", false);
+            animators[i].Rebind();
+        }
+    }
+}
